e7c525fc075552345c1148d0ce99a53e99da6d79
fvgtekken <federicovongromann@gmail.com>
2021-06-02 14:37:11 -0300

    Add scanf samples

---
+++ samples/chapter8/scanfString.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+char str[80];
+
+printf("Enter a string: ");
+
+scanf("%s", str);
+
+printf("Here's your string: %s", str);
+return 0;
+
+}
---
+++ samples/chapter8/scanfUnsigned.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+   
+   unsigned num;
+    scanf("%u", &num);
+    printf("%u", num);
+  
+    
+    return 0;
+}
