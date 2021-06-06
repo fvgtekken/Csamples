4c5ae504da8df6748b31d5e50d1b1f696ce72d3d
fvgtekken <federicovongromann@gmail.com>
2021-06-06 19:52:38 -0300

    Add Chipote sample

--- samples/chapter5/test/test3.cpp
+++ samples/chapter5/test/test3.cpp
@@ -10,16 +10,26 @@ int *tos, *p1, stack[SIZE];
 int main(void) {
 
 int value;
+stack[0]=111;
+stack[1]=222;
+stack[2]=333;
+
 tos = stack; /* tos points to the top of stack */
 p1 = stack; /* initialize p1 */
+
+//printf("Init Memory p1:%d\n ", p1);
+tos = tos+1;
+
+printf("Init Memory tos+Size:%d\n", *tos);
+
 
 do {
     
     printf("Enter value: ");
     scanf("%d", &value);
     if(value!=0) {
-         int chpote = push(value);
-        printf("%d", chpote);
+        int chpote = push(value);
+        printf("Success entered value: %d\n", chpote);
     } else {
         printf("value on top is %d\n", pop());
     }
@@ -33,12 +43,15 @@ int push(int i) {
 
     p1++;
     if(p1==(tos+SIZE)) {
+        printf("Address Memory p1:%d\n ", p1);
+        printf("Address Memory tos+Size:%d\n", tos+SIZE);
+
         printf("Stack Overflow.\n");
         exit(1);
     }
 
     *p1 = i;
-    return i;
+    return *p1;
 
 }
 
