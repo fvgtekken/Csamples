244c3fb19142c1cd8f483cd760f3afe28ab8fc58
fvgtekken <federicovongromann@gmail.com>
2021-06-08 20:52:33 -0300

    Add more function pointer

---
+++ samples/chapter5/test/test10.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+void check(char *a, char *b,
+
+int (*cmp)(const char *, const char *));
+int numcmp(const char *a, const char *b);
+
+int main(void) {
+
+    char s1[80], s2[80];
+
+    gets(s1);
+    gets(s2);
+    if(isalpha(*s1)) {
+       check(s1, s2, strcmp);  
+    } else {
+       check(s1, s2, numcmp); 
+    }
+   
+    return 0;
+}
+
+void check(char *a, char *b, int (*cmp)(const char *, const char *)) {
+
+    printf("Testing for equality.\n");
+
+    if(!(*cmp)(a, b)) {
+    printf("Equal");  
+    } else {
+    printf("Not Equal");   
+    }
+
+}
+int numcmp(const char *a, const char *b) {
+    if(atoi(a)==atoi(b)) {
+        return 0;
+    } else {
+    return 1;  
+    }
+}
