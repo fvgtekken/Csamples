d432b7f46859c442a47f6e46063a6fe7240308d8
fvgtekken <federicovongromann@gmail.com>
2021-06-13 12:42:30 -0300

    Add Read file

---
+++ samples/chapter9/demo2.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (int argc, char *argv[]) {
+
+    FILE *fp;
+    char ch;
+
+    if(argc!=2) {
+        printf("You forgot to enter the filename.\n");
+        exit(1);
+    }
+
+    if((fp=fopen(argv[1], "r"))==NULL) {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+
+    ch = getc(fp); /* read one character */
+    
+    while (ch!=EOF) {
+        putchar(ch); /* print on screen */
+        ch = getc(fp);
+    }
+    
+    
+    fclose(fp);
+    return 0;
+}
