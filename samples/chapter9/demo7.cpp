45373defd5e6632bac08ab854b491f156b703eb5
fvgtekken <federicovongromann@gmail.com>
2021-06-13 14:34:04 -0300

    Add erase example

---
+++ samples/chapter9/demo7.cpp
@@ -0,0 +1,27 @@
+/* Double check before erasing. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]) {
+char str[80];
+
+if(argc!=2) {
+    printf("usage: xerase <filename>\n");
+    exit(1);
+}
+
+printf("Erase %s? (Y/N): ", argv[1]);
+gets(str);
+
+if(toupper(*str) == 'Y') {
+    if(remove(argv[1])) {
+        printf("Cannot erase file.\n");
+        exit(1);
+    } else {
+         printf("Your file was deleted successfuly YANQUE!.\n");
+    }
+}
+
+return 0;
+}
