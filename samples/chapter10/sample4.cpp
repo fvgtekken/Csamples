69fac33e843598b0c2f4247a07e9177a3d337bd0
fvgtekken <federicovongromann@gmail.com>
2021-06-15 10:18:19 -0300

    Add Sample directive

---
+++ samples/chapter10/sample4.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define TED 10
+
+int main(void) {
+
+#ifdef TED
+    printf("Hi Ted\n");
+#else
+    printf("Hi anyone\n");
+#endif
+
+#ifndef RALPH
+    printf("RALPH not defined\n");
+#endif
+return 0;
+
+}
