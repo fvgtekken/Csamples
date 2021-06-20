d7e6883e14897a79dd6252f6e59946c761d3015c
fvgtekken <federicovongromann@gmail.com>
2021-06-20 13:33:22 -0300

    Add Sample public private

--- samples/chapter12/sample.cpp
+++ samples/chapter12/sample.cpp
@@ -1,15 +1,50 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-int main() {
-    float f;
-    char str[80];
-    double d;
-    cout << "Enter two floating point numbers: ";
-    cin >> f >> d;
-    cout << "Enter a string: ";
-    cin >> str;
-    cout << f << " " << d << " " << str;
-    return 0;
 
+
+class employee {
+    char name[80]; // private by default
+    public:
+        void putname(char n[]); // these are public
+        void getname(char n[]);
+    private:
+        double wage; // now, private again
+       
+    public:
+        void putwage(double w); // back to public
+    double getwage();
+};
+
+void employee::putname(char n[]){
+    strcpy(name, n);
+}
+
+void employee::getname(char n[]) {
+    strcpy(n, name);
+}
+
+void employee::putwage(double w){
+    wage = w;
+}
+
+double employee::getwage() {
+    return wage;
+}
+
+int main() {
+    
+    employee ted;
+    char name[80];
+    char str[80];
+    strcpy(str, "Ted Jones");
+
+    ted.putname(str);
+    ted.putwage(75000);
+
+    ted.getname(name);
+    cout << name << " makes $";
+    cout << ted.getwage() << " per year.";
+    return 0;
 }
